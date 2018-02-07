037805e79022eb7768fe159e6fd1bba9baaee7c1
Samanyou Garg <samanyugarg@gmail.com>
2018-02-07 08:50:31 +0000

    RadhaKrishna

--- config.py
+++ config.py
@@ -32,8 +32,8 @@ class Config:
 
     MAIL_SERVER = os.environ.get('MAIL_SERVER')
     MAIL_PORT = 465
-    MAIL_USE_TLS = os.environ.get('MAIL_USE_TLS') or True
-    MAIL_USE_SSL = os.environ.get('MAIL_USE_SSL') or False
+    MAIL_USE_TLS = False
+    MAIL_USE_SSL = True
     MAIL_USERNAME = os.environ.get('MAIL_USERNAME')
     MAIL_PASSWORD = os.environ.get('MAIL_PASSWORD')
     MAIL_DEFAULT_SENDER = os.environ.get('MAIL_DEFAULT_SENDER')
