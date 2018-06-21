119e34a6f902443e4841ada1753a7029e129fdf3
Samanyou Garg <samanyugarg@gmail.com>
2018-06-21 17:30:31 +0100

    राधे राधे

--- app/main/views.py
+++ app/main/views.py
@@ -1511,8 +1511,8 @@ def shloka_of_the_day_email():
 
 if not os.environ.get('DEBUG'):
     scheduler.add_job(shloka_of_the_day_radhakrishna, 'cron', hour=4, minute=30)
-    scheduler.add_job(verse_of_the_day_notification, 'cron', hour=16, minute=25)
-    scheduler.add_job(shloka_of_the_day_email, 'cron', hour=16, minute=25)
+    scheduler.add_job(verse_of_the_day_notification, 'cron', hour=16, minute=32)
+    scheduler.add_job(shloka_of_the_day_email, 'cron', hour=16, minute=32)
     scheduler.start()
     atexit.register(lambda: scheduler.shutdown())
 
