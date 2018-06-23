2ec1aca89cb2b9a000e092f2b797c719097e8055
Samanyou Garg <samanyugarg@gmail.com>
2018-06-23 17:32:36 +0100

    राधे राधे

--- app/main/views.py
+++ app/main/views.py
@@ -1573,7 +1573,7 @@ def shloka_of_the_day_email():
 # if os.environ.get('RADHA') == "KRISHNA":
 scheduler.add_job(shloka_of_the_day_radhakrishna, 'cron', hour=4, minute=30)
 scheduler.add_job(verse_of_the_day_notification, 'cron', hour=5, minute=30)
-scheduler.add_job(shloka_of_the_day_email, 'cron', hour=16, minute=32)
+scheduler.add_job(shloka_of_the_day_email, 'cron', hour=16, minute=34)
 scheduler.start()
 atexit.register(lambda: scheduler.shutdown())
 
