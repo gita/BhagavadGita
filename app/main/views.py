97eaba2449b4ddc1ccbcb30a31cdb9b4bc26423c
Samanyou Garg <samanyugarg@gmail.com>
2018-06-23 16:04:37 +0100

    राधे राधे

--- app/main/views.py
+++ app/main/views.py
@@ -1570,12 +1570,12 @@ def shloka_of_the_day_email():
             shloka_hindi=verse.meaning)
 
 
-if not os.environ.get('DEBUG'):
-    scheduler.add_job(shloka_of_the_day_radhakrishna, 'cron', hour=4, minute=30)
-    scheduler.add_job(verse_of_the_day_notification, 'cron', hour=15, minute=4)
-    scheduler.add_job(shloka_of_the_day_email, 'cron', hour=15, minute=4)
-    scheduler.start()
-    atexit.register(lambda: scheduler.shutdown())
+# if not os.environ.get('DEBUG'):
+scheduler.add_job(shloka_of_the_day_radhakrishna, 'cron', hour=4, minute=30)
+scheduler.add_job(verse_of_the_day_notification, 'cron', hour=15, minute=6)
+scheduler.add_job(shloka_of_the_day_email, 'cron', hour=15, minute=6)
+scheduler.start()
+atexit.register(lambda: scheduler.shutdown())
 
 
 @main.route('/privacy-policy/', methods=['GET'])
