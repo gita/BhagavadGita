1bf7cec8b507bbbdd4facb538b9c215ba5f3518c
Samanyou Garg <samanyugarg@gmail.com>
2019-01-04 09:08:45 +0530

    RadhaKrishna

--- app/assets.py
+++ app/assets.py
@@ -5,12 +5,12 @@ app_css = Bundle('app.scss', filters='scss', output='styles/app.css')
 app_js = Bundle('app.js', filters='jsmin', output='scripts/app.js')
 
 vendor_css = Bundle(
-    # 'vendor/bootstrap.min.css',
-    # 'vendor/mdb.min.css',
+    'vendor/bootstrap.min.css',
+    'vendor/mdb.min.css',
     'vendor/select2.min.css',
     'vendor/font-awesome.min.css',
     'vendor/bootstrap-social.min.css',
-    # 'vendor/materialize.min.css',
+    'vendor/materialize.min.css',
     output='styles/vendor.css')
 
 core_js = Bundle(
